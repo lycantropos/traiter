b945abdba99b01ecc1e2e89b044c1d6347d95383
Azat Ibrakov <azatibrakov@gmail.com>
2022-07-11 02:08:07 +0700

    Add docs for 'collections' module traits

--- src/collections/traits.rs
+++ src/collections/traits.rs
@@ -1,28 +1,83 @@
 pub trait Capacitary {
     type Capacity;
 
+    /// Returns capacity of a collection.
+    #[cfg_attr(
+        feature = "std",
+        doc = r##"
+```
+use traiter::collections::Capacitary;
+let collection = Vec::<i8>::with_capacity(10);
+assert_eq!(Capacitary::capacity(&collection), 10);
+```
+"##
+    )]
     fn capacity(&self) -> Self::Capacity;
 }
 
 pub trait Clearable {
+    /// Removes all elements from a collection.
+    #[cfg_attr(
+        feature = "std",
+        doc = r##"
+```
+use traiter::collections::{Clearable, Emptiable};
+let mut collection = vec![0];
+assert!(!Emptiable::is_empty(&collection));
+Clearable::clear(&mut collection);
+assert!(Emptiable::is_empty(&collection));
+```
+"##
+    )]
     fn clear(&mut self);
 }
 
 pub trait Container<Value> {
+    /// Checks if collection contains a value.
+    /// ```
+    /// use traiter::collections::Container;
+    /// assert!(Container::contains(&[0], &0));
+    /// assert!(!Container::contains(&[0], &1));
+    /// ```
     fn contains(&self, value: Value) -> bool;
 }
 
 pub trait Emptiable {
+    /// Checks if collection is empty.
+    /// ```
+    /// use traiter::collections::Emptiable;
+    /// assert!(Emptiable::is_empty(&[0; 0]));
+    /// assert!(!Emptiable::is_empty(&[0]));
+    /// ```
     fn is_empty(&self) -> bool;
 }
 
 pub trait Lengthsome {
     type Length;
 
+    /// Returns number of elements in a collection.
+    /// ```
+    /// use traiter::collections::Lengthsome;
+    /// assert_eq!(Lengthsome::len(&[0; 0]), 0);
+    /// assert_eq!(Lengthsome::len(&[0]), 1);
+    /// ```
     fn len(&self) -> Self::Length;
 }
 
 pub trait Reservable: Capacitary {
+    /// Returns number of elements in a collection.
+    #[cfg_attr(
+        feature = "std",
+        doc = r##"
+```
+use traiter::collections::{Capacitary, Reservable};
+let mut collection = Vec::<i8>::with_capacity(10);
+assert_eq!(Capacitary::capacity(&collection), 10);
+Reservable::reserve(&mut collection, 20);
+assert_eq!(Capacitary::capacity(&collection), 20);
+```
+"##
+    )]
     fn reserve(&mut self, additional: Self::Capacity);
 }
 
