3655ea2a5290a64354787260e3cb8827b3f5c32a
Azat Ibrakov <azatibrakov@gmail.com>
2022-07-10 15:45:08 +0700

    Add 'CheckedPow' trait

---
+++ src/numbers/checked_pow.rs
@@ -0,0 +1,20 @@
+pub trait CheckedPow<Exponent> {
+    type Output;
+
+    fn checked_pow(self, exponent: Exponent) -> Self::Output;
+}
+
+macro_rules! primitive_checked_pow_impl {
+    ($($t:ty)*) => ($(
+        impl CheckedPow<u32> for $t {
+            type Output = Option<$t>;
+
+            #[inline(always)]
+            fn checked_pow(self, exponent: u32) -> Self::Output {
+                <$t>::checked_pow(self, exponent)
+            }
+        }
+    )*)
+}
+
+primitive_checked_pow_impl!(i8 i16 i32 i64 i128 isize u8 u16 u32 u64 u128 usize);
