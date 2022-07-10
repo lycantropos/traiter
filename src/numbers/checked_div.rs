841a5e16e3ee3f8d27a93d93a78450b2e293f190
Azat Ibrakov <azatibrakov@gmail.com>
2022-07-10 15:37:05 +0700

    Add 'CheckedDiv' trait

---
+++ src/numbers/checked_div.rs
@@ -0,0 +1,20 @@
+pub trait CheckedDiv<Divisor = Self> {
+    type Output;
+
+    fn checked_div(self, divisor: Divisor) -> Self::Output;
+}
+
+macro_rules! primitive_checked_div_impl {
+    ($($t:ty)*) => ($(
+        impl CheckedDiv for $t {
+            type Output = Option<Self>;
+
+            #[inline(always)]
+            fn checked_div(self, divisor: Self) -> Self::Output {
+                <$t>::checked_div(self, divisor)
+            }
+        }
+    )*)
+}
+
+primitive_checked_div_impl!(i8 i16 i32 i64 i128 isize u8 u16 u32 u64 u128 usize);
