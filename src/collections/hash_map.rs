1aa7132d669b9bee7ea112af8ebb227640224807
Azat Ibrakov <azatibrakov@gmail.com>
2022-07-10 13:56:29 +0700

    Implement 'TryReservable' trait for 'HashMap' generic struct

--- src/collections/hash_map.rs
+++ src/collections/hash_map.rs
@@ -1,7 +1,9 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, TryReserveError};
 use std::hash::{BuildHasher, Hash};
 
-use super::traits::{Capacitary, Clearable, Container, Emptiable, Lengthsome, Reservable};
+use super::traits::{
+    Capacitary, Clearable, Container, Emptiable, Lengthsome, Reservable, TryReservable,
+};
 
 impl<Key, Value, State> Capacitary for HashMap<Key, Value, State> {
     type Capacity = usize;
@@ -40,5 +42,13 @@ impl<Key, Value, State> Lengthsome for HashMap<Key, Value, State> {
 impl<Key: Eq + Hash, Value, State: BuildHasher> Reservable for HashMap<Key, Value, State> {
     fn reserve(&mut self, additional: Self::Capacity) {
         HashMap::reserve(self, additional)
+    }
+}
+
+impl<Key: Eq + Hash, Value, State: BuildHasher> TryReservable for HashMap<Key, Value, State> {
+    type Error = TryReserveError;
+
+    fn try_reserve(&mut self, additional: Self::Capacity) -> Result<(), Self::Error> {
+        HashMap::try_reserve(self, additional)
     }
 }
