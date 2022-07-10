66059765006fff39d2ed7accb3a7211822838626
Azat Ibrakov <azatibrakov@gmail.com>
2022-07-10 14:44:35 +0700

    Add 'FromBytes' trait

---
+++ src/numbers/from_bytes.rs
@@ -0,0 +1,23 @@
+use std::convert::TryInto;
+
+use super::types::Endianness;
+
+pub trait FromBytes {
+    fn from_bytes(bytes: &[u8], endianness: Endianness) -> Self;
+}
+
+macro_rules! primitive_from_bytes_impl {
+    ($($t:ty)*) => ($(
+        impl FromBytes for $t {
+            #[inline(always)]
+            fn from_bytes(bytes: &[u8], endianness: Endianness) -> Self {
+                match endianness {
+                   Endianness::Big => <$t>::from_be_bytes(bytes.try_into().unwrap()),
+                   Endianness::Little => <$t>::from_le_bytes(bytes.try_into().unwrap()),
+                }
+            }
+        }
+    )*)
+}
+
+primitive_from_bytes_impl!(i8 i16 i32 i64 i128 isize u8 u16 u32 u64 u128 usize);
--- src/numbers/mod.rs
+++ src/numbers/mod.rs
@@ -1,5 +1,9 @@
 pub use self::abs::Abs;
 pub use self::ceil::Ceil;
+pub use self::from_bytes::FromBytes;
+pub use self::types::Endianness;
 
 mod abs;
 mod ceil;
+mod from_bytes;
+mod types;
