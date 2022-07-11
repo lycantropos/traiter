a18f5033338bf642ae101eaac4c22b5884fb54f4
Azat Ibrakov <azatibrakov@gmail.com>
2022-07-11 23:40:15 +0700

    Add docs for 'CheckedDivRem' trait

--- src/numbers/checked_div_rem.rs
+++ src/numbers/checked_div_rem.rs
@@ -3,6 +3,22 @@ use core::ops::{Div, Rem};
 pub trait CheckedDivRem<Divisor = Self> {
     type Output;
 
+    /// Returns checked quotient and remainder of division.
+    /// ```
+    /// use traiter::numbers::CheckedDivRem;
+    /// // signed integers
+    /// assert_eq!(
+    ///     CheckedDivRem::checked_div_rem(-3i8, 2i8), Some((-1i8, -1i8))
+    /// );
+    /// assert_eq!(
+    ///     CheckedDivRem::checked_div_rem(-3i8, 1i8), Some((-3i8, 0i8))
+    /// );
+    /// assert_eq!(CheckedDivRem::checked_div_rem(-3i8, 0i8), None);
+    /// // unsigned integers
+    /// assert_eq!(CheckedDivRem::checked_div_rem(3u8, 2u8), Some((1u8, 1u8)));
+    /// assert_eq!(CheckedDivRem::checked_div_rem(3u8, 1u8), Some((3u8, 0u8)));
+    /// assert_eq!(CheckedDivRem::checked_div_rem(3u8, 0u8), None);
+    /// ```
     fn checked_div_rem(self, divisor: Divisor) -> Self::Output;
 }
 
