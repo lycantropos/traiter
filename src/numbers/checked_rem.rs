24bc72630cd370bb9928772eff9e53d55e3b8775
Azat Ibrakov <azatibrakov@gmail.com>
2022-07-11 22:30:45 +0700

    Add 'CheckedRem' trait

---
+++ src/numbers/checked_rem.rs
@@ -0,0 +1,34 @@
+pub trait CheckedRem<Divisor = Self> {
+    type Output;
+
+    /// Returns checked remainder of integer division.
+    /// ```
+    /// use traiter::numbers::CheckedRem;
+    /// // signed integers
+    /// assert_eq!(CheckedRem::checked_rem(-3i8, 2i8), Some(-1i8));
+    /// assert_eq!(CheckedRem::checked_rem(-3i8, 1i8), Some(0i8));
+    /// assert_eq!(CheckedRem::checked_rem(-3i8, 0i8), None);
+    /// // unsigned integers
+    /// assert_eq!(CheckedRem::checked_rem(3u8, 2u8), Some(1u8));
+    /// assert_eq!(CheckedRem::checked_rem(3u8, 1u8), Some(0u8));
+    /// assert_eq!(CheckedRem::checked_rem(3u8, 0u8), None);
+    /// ```
+    fn checked_rem(self, divisor: Divisor) -> Self::Output;
+}
+
+macro_rules! primitive_checked_rem_impl {
+    ($($t:ty)*) => ($(
+        impl CheckedRem for $t {
+            type Output = Option<Self>;
+
+            #[inline(always)]
+            fn checked_rem(self, divisor: Self) -> Self::Output {
+                <$t>::checked_rem(self, divisor)
+            }
+        }
+    )*)
+}
+
+primitive_checked_rem_impl!(
+    i8 i16 i32 i64 i128 isize u8 u16 u32 u64 u128 usize
+);
