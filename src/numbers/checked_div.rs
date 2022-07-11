dda17fbeabf356352857bddfee142461c63cfeaa
Azat Ibrakov <azatibrakov@gmail.com>
2022-07-11 23:31:10 +0700

    Add docs for 'CheckedDiv' trait

--- src/numbers/checked_div.rs
+++ src/numbers/checked_div.rs
@@ -1,6 +1,18 @@
 pub trait CheckedDiv<Divisor = Self> {
     type Output;
 
+    /// Returns checked quotient of division.
+    /// ```
+    /// use traiter::numbers::CheckedDiv;
+    /// // signed integers
+    /// assert_eq!(CheckedDiv::checked_div(-3i8, 2i8), Some(-1i8));
+    /// assert_eq!(CheckedDiv::checked_div(-3i8, 1i8), Some(-3i8));
+    /// assert_eq!(CheckedDiv::checked_div(-3i8, 0i8), None);
+    /// // unsigned integers
+    /// assert_eq!(CheckedDiv::checked_div(3u8, 2u8), Some(1u8));
+    /// assert_eq!(CheckedDiv::checked_div(3u8, 1u8), Some(3u8));
+    /// assert_eq!(CheckedDiv::checked_div(3u8, 0u8), None);
+    /// ```
     fn checked_div(self, divisor: Divisor) -> Self::Output;
 }
 
