6fd5f4668b4dfd5d45b3453da05bb2e9c89e96dc
Azat Ibrakov <azatibrakov@gmail.com>
2022-07-11 11:57:11 +0700

    Complete implementation of 'BitLength' trait of primitive types

--- src/numbers/bit_length.rs
+++ src/numbers/bit_length.rs
@@ -4,7 +4,22 @@ pub trait BitLength {
     fn bit_length(self) -> Self::Output;
 }
 
-macro_rules! primitive_bit_length_impl {
+macro_rules! primitive_signed_bit_length_impl {
+    ($($t:ty)*) => ($(
+        impl BitLength for $t {
+            type Output = usize;
+
+            #[inline(always)]
+            fn bit_length(self) -> Self::Output {
+                (<$t>::BITS as usize) - (self.abs().leading_zeros() as usize)
+            }
+        }
+    )*)
+}
+
+primitive_signed_bit_length_impl!(i8 i16 i32 i64 i128 isize);
+
+macro_rules! primitive_unsigned_bit_length_impl {
     ($($t:ty)*) => ($(
         impl BitLength for $t {
             type Output = usize;
@@ -17,4 +32,4 @@ macro_rules! primitive_bit_length_impl {
     )*)
 }
 
-primitive_bit_length_impl!(i8 i16 i32 i64 i128 isize u8 u16 u32 u64 u128 usize);
+primitive_unsigned_bit_length_impl!(u8 u16 u32 u64 u128 usize);
