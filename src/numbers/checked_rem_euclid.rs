08f6ac130240624a02577aa1c27954df77a1dec7
Azat Ibrakov <azatibrakov@gmail.com>
2022-07-11 23:25:04 +0700

    Add 'CheckedRemEuclid' trait

---
+++ src/numbers/checked_rem_euclid.rs
@@ -0,0 +1,34 @@
+pub trait CheckedRemEuclid<Divisor = Self> {
+    type Output;
+
+    /// Returns checked euclidean modulo.
+    /// ```
+    /// use traiter::numbers::CheckedRemEuclid;
+    /// // signed integers
+    /// assert_eq!(CheckedRemEuclid::checked_rem_euclid(-3i8, 2i8), Some(1i8));
+    /// assert_eq!(CheckedRemEuclid::checked_rem_euclid(-3i8, 1i8), Some(0i8));
+    /// assert_eq!(CheckedRemEuclid::checked_rem_euclid(-3i8, 0i8), None);
+    /// // unsigned integers
+    /// assert_eq!(CheckedRemEuclid::checked_rem_euclid(3u8, 2u8), Some(1u8));
+    /// assert_eq!(CheckedRemEuclid::checked_rem_euclid(3u8, 1u8), Some(0u8));
+    /// assert_eq!(CheckedRemEuclid::checked_rem_euclid(3u8, 0u8), None);
+    /// ```
+    fn checked_rem_euclid(self, divisor: Divisor) -> Self::Output;
+}
+
+macro_rules! primitive_checked_rem_euclid_impl {
+    ($($t:ty)*) => ($(
+        impl CheckedRemEuclid for $t {
+            type Output = Option<Self>;
+
+            #[inline(always)]
+            fn checked_rem_euclid(self, divisor: Self) -> Self::Output {
+                <$t>::checked_rem_euclid(self, divisor)
+            }
+        }
+    )*)
+}
+
+primitive_checked_rem_euclid_impl!(
+    i8 i16 i32 i64 i128 isize u8 u16 u32 u64 u128 usize
+);
