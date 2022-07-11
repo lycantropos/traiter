7886fd96bcb5dd2863682e16e94c7da069f59e5f
Azat Ibrakov <azatibrakov@gmail.com>
2022-07-11 14:34:16 +0700

    Add docs for 'ToBytes' trait

--- src/numbers/to_bytes.rs
+++ src/numbers/to_bytes.rs
@@ -5,6 +5,18 @@ use super::types::Endianness;
 pub trait ToBytes {
     type Output;
 
+    /// Returns representation of a number as a byte array in given endianness.
+    /// ```
+    /// use traiter::numbers::{ToBytes, Endianness};
+    /// /// signed integers
+    /// assert_eq!(ToBytes::to_bytes(&-1i8, Endianness::Little), [255u8]);
+    /// assert_eq!(ToBytes::to_bytes(&0i8, Endianness::Little), [0u8]);
+    /// assert_eq!(ToBytes::to_bytes(&1i8, Endianness::Little), [1u8]);
+    /// /// unsigned integers
+    /// assert_eq!(ToBytes::to_bytes(&0u8, Endianness::Little), [0u8]);
+    /// assert_eq!(ToBytes::to_bytes(&1u8, Endianness::Little), [1u8]);
+    /// assert_eq!(ToBytes::to_bytes(&2u8, Endianness::Little), [2u8]);
+    /// ```
     fn to_bytes(&self, endianness: Endianness) -> Self::Output;
 }
 
