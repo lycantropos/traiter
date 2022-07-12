fba62c8bdd4f2f7a5af5d0be03b6b9fcc2e39d8c
Azat Ibrakov <azatibrakov@gmail.com>
2022-07-12 11:34:09 +0700

    Complete enums

--- src/numbers/types.rs
+++ src/numbers/types.rs
@@ -1,10 +1,10 @@
-#[derive(Clone, Copy, Eq, PartialEq)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub enum Endianness {
     Big,
     Little,
 }
 
-#[derive(Clone, Copy, Eq, PartialEq)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub enum TieBreaking {
     AwayFromZero,
     ToEven,
