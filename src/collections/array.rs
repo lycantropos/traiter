c66860fc04b3191594747a4eaa3a7a4f1291041f
Azat Ibrakov <azatibrakov@gmail.com>
2022-12-26 06:02:56 +0100

    Implement 'MutIterable' trait for arrays

--- src/collections/array.rs
+++ src/collections/array.rs
@@ -1,6 +1,6 @@
-use core::slice::Iter;
+use core::slice::{Iter, IterMut};
 
-use super::traits::{Container, Emptiable, Iterable, Lengthsome};
+use super::traits::{Container, Emptiable, Iterable, Lengthsome, MutIterable};
 
 impl<Element: PartialEq, const LENGTH: usize> Container<&Element>
     for [Element; LENGTH]
@@ -29,5 +29,15 @@ impl<Element, const LENGTH: usize> Lengthsome for [Element; LENGTH] {
 
     fn len(&self) -> Self::Length {
         LENGTH
+    }
+}
+
+impl<'a, Element: 'a, const LENGTH: usize> MutIterable<'a>
+    for [Element; LENGTH]
+{
+    type Output = IterMut<'a, Element>;
+
+    fn iter_mut(&'a mut self) -> Self::Output {
+        self.as_mut_slice().iter_mut()
     }
 }
