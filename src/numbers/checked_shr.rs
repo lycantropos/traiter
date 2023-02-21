3a1d859945d601846a0d25fb3b87be00baba9be1
Azat Ibrakov <azatibrakov@gmail.com>
2023-02-21 11:35:05 +0100

    Complete 'CheckedShr' trait implementation

--- src/numbers/checked_shr.rs
+++ src/numbers/checked_shr.rs
@@ -1,5 +1,3 @@
-use core::convert::TryFrom;
-
 pub trait CheckedShr<Shift = Self> {
     type Output;
 
@@ -7,65 +5,28 @@ pub trait CheckedShr<Shift = Self> {
     /// ```
     /// use traiter::numbers::CheckedShr;
     /// // signed integers
-    /// assert_eq!(CheckedShr::checked_shr(-3i8, -1i8), None);
-    /// assert_eq!(CheckedShr::checked_shr(-3i8, 0i8), Some(-3i8));
-    /// assert_eq!(CheckedShr::checked_shr(-3i8, 1i8), Some(-2i8));
+    /// assert_eq!(CheckedShr::checked_shr(-3i8, 0u32), Some(-3i8));
+    /// assert_eq!(CheckedShr::checked_shr(-3i8, 1u32), Some(-2i8));
+    /// assert_eq!(CheckedShr::checked_shr(-3i8, 2u32), Some(-1i8));
     /// // unsigned integers
-    /// assert_eq!(CheckedShr::checked_shr(3u8, 0u8), Some(3u8));
-    /// assert_eq!(CheckedShr::checked_shr(3u8, 1u8), Some(1u8));
-    /// assert_eq!(CheckedShr::checked_shr(3u8, 2u8), Some(0u8));
+    /// assert_eq!(CheckedShr::checked_shr(3u8, 0u32), Some(3u8));
+    /// assert_eq!(CheckedShr::checked_shr(3u8, 1u32), Some(1u8));
+    /// assert_eq!(CheckedShr::checked_shr(3u8, 2u32), Some(0u8));
     /// ```
     fn checked_shr(self, shift: Shift) -> Self::Output;
 }
 
-macro_rules! signed_integer_checked_shr_impl {
-    ($base:ty, $shift:ty) => {
-        impl CheckedShr<$shift> for $base {
-            type Output = Option<$base>;
-
-            #[inline(always)]
-            fn checked_shr(self, shift: $shift) -> Self::Output {
-                if shift < 0 {
-                    None
-                } else {
-                    <$base>::checked_shr(self, u32::try_from(shift).ok()?)
-                }
-            }
-        }
-    };
-}
-
-macro_rules! unsigned_integer_checked_shr_impl {
-    ($base:ty, $shift:ty) => {
-        impl CheckedShr<$shift> for $base {
+macro_rules! integer_checked_shr_impl {
+    ($($integer:ty)*) => ($(
+        impl CheckedShr<u32> for $integer {
             type Output = Option<Self>;
 
             #[inline(always)]
-            fn checked_shr(self, shift: $shift) -> Self::Output {
-                <$base>::checked_shr(self, u32::try_from(shift).ok()?)
+            fn checked_shr(self, shift: u32) -> Self::Output {
+                <$integer>::checked_shr(self, shift)
             }
         }
-    };
-}
-
-macro_rules! integer_checked_shr_impl {
-    ($($base:ty)*) => ($(
-        signed_integer_checked_shr_impl! { $base, i8 }
-        signed_integer_checked_shr_impl! { $base, i16 }
-        signed_integer_checked_shr_impl! { $base, i32 }
-        signed_integer_checked_shr_impl! { $base, i64 }
-        signed_integer_checked_shr_impl! { $base, i128 }
-        signed_integer_checked_shr_impl! { $base, isize }
-
-        unsigned_integer_checked_shr_impl! { $base, u8 }
-        unsigned_integer_checked_shr_impl! { $base, u16 }
-        unsigned_integer_checked_shr_impl! { $base, u32 }
-        unsigned_integer_checked_shr_impl! { $base, u64 }
-        unsigned_integer_checked_shr_impl! { $base, u128 }
-        unsigned_integer_checked_shr_impl! { $base, usize }
     )*)
 }
 
-integer_checked_shr_impl!(
-    i8 i16 i32 i64 i128 isize u8 u16 u32 u64 u128 usize
-);
+integer_checked_shr_impl!(i8 i16 i32 i64 i128 isize u8 u16 u32 u64 u128 usize);
